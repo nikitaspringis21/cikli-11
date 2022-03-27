fb2646fd8d5a93a8e7f7459d1dbd8d522f009019
nikita springis <102158983+nekitoslicejs@users.noreply.github.com>
2022-03-27 22:32:32 +0300

    Добавил нахождение минимальной и максимально

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,6 +14,8 @@ public class Main {
         int last_digit_amount = 0;
         int sum = 0;
         int mult = 1;
+        int max_digit = last_digit;
+        int min_digit = last_digit;
 
         while (number > 0){
             int digit = number % 10; // каждую следующую цифру заносим в эту переменную
@@ -21,6 +23,8 @@ public class Main {
             if (digit % 10 == last_digit) last_digit_amount++;
             sum += digit;
             mult *= digit;
+            if (max_digit < digit) max_digit = digit;
+            if (min_digit > digit) min_digit = digit;
             number /= 10; // в конце каждой итерации "отрезаем" от числа последнюю цифру
         }
 
@@ -28,6 +32,8 @@ public class Main {
         System.out.println(last_digit_amount);
         System.out.println(sum);
         System.out.println(mult);
+        System.out.println(max_digit);
+        System.out.println(min_digit);
 
 
 
